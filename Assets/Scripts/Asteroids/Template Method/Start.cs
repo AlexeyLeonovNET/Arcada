b3097d9fcd30d08246c2f2c1ece21dfccb189af6
Alexey Leonov <x1leon1x@gmail.com>
2022-12-17 04:07:29 +0300

    Observer, State, Strategy

---
+++ Assets/Scripts/Asteroids/Template Method/Start.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+namespace BehavioralPatterns.TemplateMethod.ExampleFirst
+{
+    public sealed class TemplateMethodTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            var vk = new VK();
+            vk.Draw();
+        }
+    }
+}
+
